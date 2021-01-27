b4107b6d0bc87d46a9ce49dc997793b07aef1e0c
Chandan Singh <chandan_singh@berkeley.edu>
2021-01-27 00:22:24 -0500

    generate pipeline names

--- pcsp/module_set.py
+++ pcsp/module_set.py
@@ -4,7 +4,7 @@ Function arguments are each a list
 
 
 def s(x):
-    if type(x) == list:
+    if type(x) in [list, tuple]:
         return len(x)
     else:
         return x.shape
@@ -35,18 +35,23 @@ class ModuleSet:
         results: Tuple[List]
             e.g. ([x1, x2, x3], [y1, y2, y3])
         '''
-        print('intro args', len(args))
+        # print('intro args', s(args))
+        # if len(args) == 1:
+        #     args = args[0]
         # args = args[0]
-        print('args_tuple', len(args), len(args[0]), 'kwargs', kwargs)  # , args[0][0].shape)
-        args_list = to_list(args)  # [[x1, y1], [x2, y2], [x3, y3]]
-        print('args_list', len(args_list), len(args_list[0]))  # , args_list[0][0].shape, 'kwargs', kwargs)
+        # print('args_tuple', s(args), s(args[0]), 'kwargs', kwargs)  # , args[0][0].shape)
+        # args should look like ([x1, x2, x3], [y1, y2, y3])
+        args_list = to_list(args)
+        # args_list should look like [[x1, y1], [x2, y2], [x3, y3]]
+        # print('args_list', s(args_list), s(args_list[0]))  # , args_list[0][0].shape, 'kwargs', kwargs)
         results = []
         for arg in args_list:
-            print('arg', len(arg), s(arg[0]), s(arg[1]))  # , arg[0].shape)  # , arg[1].shape)
+            # print('arg', s(arg), s(arg[0]))  # , s(arg[0]), s(arg[1]))  # , arg[0].shape)  # , arg[1].shape)
             for func in funcs:
                 result = func(*arg)
                 results.append(result)
-                # print('result', s(result), s(result[0]), s(result[1]), s(result[0][0]))
+        # print(results)
+        # print('result', s(result), s(result[0]), s(result[1]), s(result[0][0]))
         # print('len(results)', len(results), len(results[0]), s(results[0][0]), s(results[0][1]))
         return to_tuple(results)
 
@@ -60,32 +65,27 @@ class ModuleSet:
     def predict(self, *args, **kwargs):
         funcs = [mod.predict for mod in self.modules]
         return self.apply_func(funcs, *args, **kwargs)
-        # results = []
-        # for mod in self.modules:
-        #     result = mod.predict(*args, **kwargs)
-        #     results.append(result)
-        # return results
 
     def predict_proba(self, *args, **kwargs):
         funcs = [mod.predict for mod in self.modules]
         return self.apply_func(funcs, *args, **kwargs)
-        # results = []
-        # for mod in self.modules:
-        #     result = mod.predict(*args, **kwargs)
-        #     results.append(result)
-        # return results
+
+    def repeat(self, x):
+        '''
+
+        Parameters
+        ----------
+        x: list to be repeated
+
+        Returns
+        -------
+        List repeated number of times as self.modules
+        '''
+
+        return x * len(self.modules)
 
     def __call__(self, *args, **kwargs):
-        # print('len(funcs)', len(funcs), funcs)
         return self.apply_func(self.modules, *args, **kwargs)
-
-        '''
-        results = []
-        for mod_num, mod in enumerate(self.modules):
-            result = mod(*args, **kwargs)
-            results.append(result)
-        return unpack_to_tuple(results)
-        '''
 
     def __getitem__(self, i):
         '''Accesses ith item in the module set
@@ -103,10 +103,13 @@ def to_tuple(lists: list):
     '''Convert from lists to unpacked  tuple
     Ex. [[x1, y1], [x2, y2], [x3, y3]] -> ([x1, x2, x3], [y1, y2, y3])
     Ex. [[x1, y1]] -> ([x1], [y1])
+    Ex. [m1, m2, m3] -> [m1, m2, m3]
     Allows us to write X, y = ([x1, x2, x3], [y1, y2, y3])
     '''
     n_mods = len(lists)
     if n_mods <= 1:
+        return lists
+    if not type(lists[0]) == list:
         return lists
     n_tup = len(lists[0])
     tup = [[] for _ in range(n_tup)]
@@ -120,11 +123,14 @@ def to_list(tup: tuple):
     '''Convert from tuple to packed list
     Ex. ([x1, x2, x3], [y1, y2, y3]) -> [[x1, y1], [x2, y2], [x3, y3]]
     Ex. ([x1], [y1]) -> [[x1, y1]]
+    Ex. ([x1, x2, x3]) -> [[x1], [x2], [x3]]
     Allows us to call function with arguments in a loop
     '''
     n_tup = len(tup)
-    if n_tup <= 1:
+    if n_tup == 0:
         return tup
+    elif n_tup == 1:
+        return [[x] for x in tup[0]]
     n_mods = len(tup[0])
     lists_packed = [[] for _ in range(n_mods)]
     for i in range(n_mods):
--- pcsp/pipeline.py
+++ pcsp/pipeline.py
@@ -1,13 +1,14 @@
 '''Class that stores the entire pipeline of steps in a data-science workflow
 '''
+import itertools
+
+import pandas as pd
 
 
 class PCSPipeline:
     def __init__(self, steps: list = []):
         self.steps = steps
         self.cache = []
-
-
 
     def run(self, *args, **kwargs):
         '''Runs the pipeline
@@ -20,8 +21,6 @@ class PCSPipeline:
             outputs = step(*args, **kwargs)
             self.cache.append((step_name, outputs))
 
-
-
     def __getitem__(self, i):
         '''Accesses ith step of pipeline
         '''
@@ -29,3 +28,17 @@ class PCSPipeline:
 
     def __len__(self):
         return len(self.steps)
+
+    def generate_names(self, as_pandas=True):
+        name_lists = []
+        if as_pandas:
+            for step in self.steps:
+                name_lists.append([f'{i}_{str(mod)[:8]}'
+                                   for i, mod in enumerate(step)])
+            indexes = list(itertools.product(*name_lists))
+            return pd.DataFrame(indexes, columns=[step.name for step in self.steps])
+        else:
+            for step in self.steps:
+                name_lists.append([f'{step.name}_{i}_{str(mod)[:8]}'
+                                   for i, mod in enumerate(step)])
+            return list(itertools.product(*name_lists))
