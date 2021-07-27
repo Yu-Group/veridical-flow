b2cfbc923bac70d469d123b182e7d2d9ab5f928e
Rushk014 <rush.kapoor@berkeley.edu>
2021-07-26 21:49:07 -0700

    add exhaustive tests for combine_three_dicts

--- tests/test_three_dict_pipeline.py
+++ tests/test_three_dict_pipeline.py
@@ -1,3 +1,4 @@
+from pcsp.convert import combine_three_subset_dicts, extend_dicts, full_combine_two_dicts
 import pcsp
 import numpy as np
 from sklearn.linear_model import LogisticRegression
@@ -13,10 +14,43 @@ from sklearn.model_selection import train_test_split
 from sklearn.datasets import make_classification
 import pandas as pd
 
-class TestPipelines():
+class TestThreeDictPipeline():
     
     def setup(self):
         pass
+
+    def test_extend_dict(self):
+        arg_names = ['x_train', 'y_train', 'classifiers']
+        vals_tuple = ('x_train', 'y_train', ['log_reg'])
+        x_train, y_train, classifiers = init_args(vals_tuple, names=arg_names)
+        combined_dict = extend_dicts(x_train, y_train, classifiers)
+        for arg in arg_names:
+            assert arg in combined_dict.keys(), 'dict should contain ' + str(arg) + ' as key'
+        for val in vals_tuple:
+            assert val in combined_dict.values(), 'dict should contain ' + str(val) + ' as val'
+    
+    def test_full_combine_dicts(self):
+        x_train, y_train = init_args(('x_train', 'y_train'), names=['x_train', 'y_train'])
+        merged_dict = extend_dicts(x_train, y_train)
+        non_merged = {('classifier 1', 'rf'): lambda x: x, ('classifier 2', 'dt'): lambda x: x}
+        combined_dict = full_combine_two_dicts(merged_dict, non_merged)
+        k1 = (('classifier 1', 'rf'), 'x_train', 'y_train')
+        k2 = (('classifier 2', 'dt'), 'x_train', 'y_train')
+        for k in [k1, k2]:
+            assert k in combined_dict.keys(), 'dict should contain ' + str(k) + ' as key'
+    
+    def test_combine_three_dicts(self):
+        x_train, y_train = init_args(('x_train', 'y_train'), names=['x_train', 'y_train'])
+        classifiers = {('classifier 1', 'rf'): 0, ('classifier 2', 'dt'): 1}
+        combined_dict = combine_three_subset_dicts(x_train, y_train, classifiers)
+        k1 = (('classifier 1', 'rf'), 'x_train', 'y_train')
+        k2 = (('classifier 2', 'dt'), 'x_train', 'y_train')
+        for k in [k1, k2]:
+            assert k in combined_dict.keys(), 'dict should contain ' + str(k) + ' as key'
+        v1 = [0, 'x_train', 'y_train']
+        v2 = [1, 'x_train', 'y_train']
+        for v in [v1, v2]:
+            assert v in combined_dict.values(), 'dict should contain ' + str(v) + ' as value'
     
     def test_feature_importance(self):
         '''Simplest synthetic pipeline for feature importance
@@ -52,8 +86,7 @@ class TestPipelines():
         importances = feature_importance_set.evaluate(modeling_set.out, X_test, y_test)
 
         G = build_graph(importances, draw=True)
-        
-        print(importances.keys())
+
         # asserts
         k1 = (('X_train', 'y_train', 'subsampling_0', 'LR'), 'X_test', 'y_test', 'permutation_importance')
         assert k1 in importances, 'hard metrics should have ' + str(k1) + ' as key'
